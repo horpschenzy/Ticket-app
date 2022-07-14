fa560e24e24a98fb40cdd19b7935dfb939d2f853
feranmi <feranmiolasunkanmi91@gmail.com>
2022-07-14 14:28:54 +0100

    auth link to frontend

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/signin', [HomeController::class, 'signin'])->name('login');
 Route::post('/login', [AuthController::class, 'customLogin'])->name('signin');
 Route::get('/signup', [App\Http\Controllers\HomeController::class, 'signup'])->name('signup');
 
+Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
     Route::get('/department', [App\Http\Controllers\HomeController::class, 'department'])->name('department');
     Route::post('/add-department', [DepartmentController::class, 'addDepartment'])->name('add.department');
@@ -29,12 +30,4 @@ Route::get('/signup', [App\Http\Controllers\HomeController::class, 'signup'])->n
     Route::get('/users', [App\Http\Controllers\HomeController::class, 'users'])->name('users');
     Route::post('/add-user', [UserController::class, 'addUser'])->name('add.user');
     Route::get('/add-user', [UserController::class, 'addUserView'])->name('user.view');
-
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
-//     Route::get('/department', [App\Http\Controllers\HomeController::class, 'department'])->name('department');
-//     Route::post('/add-department', [DepartmentController::class, 'addDepartment'])->name('add.department');
-//     Route::get('/add-department', [DepartmentController::class, 'addDepartmentView'])->name('department.view');
-//     Route::get('/ticket', [App\Http\Controllers\HomeController::class, 'ticket'])->name('ticket');
-//     Route::get('/users', [App\Http\Controllers\HomeController::class, 'users'])->name('users');
-// });
+});
