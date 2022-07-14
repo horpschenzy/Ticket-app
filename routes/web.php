5a69dd5350a09e93917e2211c01c72470365e4d8
horpschenzy <opeoluwa.lanre@gmail.com>
2022-07-14 01:05:36 +0100

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/signin', [HomeController::class, 'signin'])->name('signin');
+Route::get('/signup', [App\Http\Controllers\HomeController::class, 'signup'])->name('signup');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
+Route::get('/department', [App\Http\Controllers\HomeController::class, 'department'])->name('department');
+Route::get('/ticket', [App\Http\Controllers\HomeController::class, 'ticket'])->name('ticket');
+Route::get('/users', [App\Http\Controllers\HomeController::class, 'users'])->name('users');
